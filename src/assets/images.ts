8aee356f470a8646d48d2132a347c04f05e9b9ab
Artem Yatsyshyn <artem.yatsyshyn@litslink.com>
2022-09-02 12:03:56 +0300

    add fractions images

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -5,4 +5,20 @@ export const images = {
   vikingsSmallIcon: require("./vikings/vikingsSmallIcon.png"),
   vikingsMiddleIcon: require("./vikings/vikingsMiddleIcon.png"),
   vikingsBigIcon: require("./vikings/vikingsBigIcon.png"),
+
+  normansSmallIcon: require("./vikings/normansSmallIcon.png"),
+  normansMiddleIcon: require("./vikings/normansMiddleIcon.png"),
+  normansBigIcon: require("./vikings/normansBigIcon.png"),
+
+  welshSmallIcon: require("./vikings/welshSmallIcon.png"),
+  welshMiddleIcon: require("./vikings/welshMiddleIcon.png"),
+  welshBigIcon: require("./vikings/welshBigIcon.png"),
+
+  celticSmallIcon: require("./vikings/celticSmallIcon.png"),
+  celticMiddleIcon: require("./vikings/celticMiddleIcon.png"),
+  celticBigIcon: require("./vikings/celticBigIcon.png"),
+
+  angloSmallIcon: require("./vikings/angloSmallIcon.png"),
+  angloMiddleIcon: require("./vikings/angloMiddleIcon.png"),
+  angloBigIcon: require("./vikings/angloBigIcon.png"),
 }
